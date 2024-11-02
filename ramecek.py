9b0024182b4a9ce925ce6097d00f9ec35b2c991d
tada007 <61982461+tada007@users.noreply.github.com>
2024-11-02 13:56:25 +0100

    Update ramecek.py

--- ramecek.py
+++ ramecek.py
@@ -40,6 +40,7 @@ with col2:
         selected_logo = logo_pfad2
         st.image(logo2_preview, caption="Logo 2")
 
+logo_percentage = st.slider('Logo-Größe (in % des Bildes)', min_value=5, max_value=30, value=10)
 # Funktion für das Hinzufügen des Rahmens und des Logos
 def add_split_frame(image, logo_path=None, logo_percentage=10, logo_position="Unten rechts"):
     border_size = 8
